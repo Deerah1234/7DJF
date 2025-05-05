85cbdac703b4c6ba83095dcd95b84751ff15ee75
Deerah1234 <chiderajames55@gmail.com>
2025-05-05 14:48:08 +0100

    update: Bookmockup

--- src/components/BookMockup.jsx
+++ src/components/BookMockup.jsx
@@ -2,25 +2,28 @@ import { bookMockupLg, bookMockupMd, bookMockupTb } from "@/assets";
 
 const BookMockup = () => {
     return (
-        <>
-            <img
-                src={bookMockupLg}
-                alt="7-Days Journey to Freedom Book"
-                className="w-full object-cover hidden lg:flex"
+        <picture className="">
+            {/* Large screens */}
+            <source
+                srcSet={bookMockupLg}
+                // className="w-full object-cover"
+                media="(min-width: 1024px)"
             />
 
+            {/* Medium screens */}
+            <source
+                srcSet={bookMockupTb}
+                // className="w-full object-cover"
+                media="(min-width: 640px)"
+            />
+
+            {/* Small screens (default) */}
             <img
                 src={bookMockupMd}
                 alt="7-Days Journey to Freedom Book"
-                className="w-full object-cover hidden md:flex lg:hidden"
+                // className="w-full object-cover"
             />
-
-            <img
-                src={bookMockupTb}
-                alt="7-Days Journey to Freedom Book"
-                className="w-full object-cover md:hidden flex"
-            />
-        </>
+        </picture>
     );
 };
 
